2647950c74063876ab6b2e5380305fab2004f0b1
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-01 19:07:00 -0700

    Implement more methods for flipping dics.

--- src/events.rs
+++ src/events.rs
@@ -54,9 +54,9 @@ trait Handler {
     fn is_valid_move(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
     fn flip_discs(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
     fn try_flipping_up(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
-    // fn try_flipping_down(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
-    // fn try_flipping_left(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
-    // fn try_flipping_right(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
+    fn try_flipping_down(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
+    fn try_flipping_left(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
+    fn try_flipping_right(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool;
     // fn try_flipping_up_left(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
     // fn try_flipping_up_right(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
     // fn try_flipping_down_left(board: &Board, turn: Turn, row: usize, col: usize) -> bool;
@@ -113,6 +113,9 @@ impl Handler for Move {
 
     fn flip_discs(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool {
         Self::try_flipping_up(board, turn, row, col)
+            || Self::try_flipping_down(board, turn, row, col)
+            || Self::try_flipping_left(board, turn, row, col)
+            || Self::try_flipping_right(board, turn, row, col)
     }
 
     fn try_flipping_up(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool {
@@ -153,6 +156,166 @@ impl Handler for Move {
                             } else if board.board[row - count][col] == Cell::White {
                                 (1..count).for_each(|r| {
                                     board.board[row - r][col] = Cell::White;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                _ => true,
+            },
+        }
+    }
+
+    fn try_flipping_down(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool {
+        println!("row = {}, col = {}", row, col);
+        match (row, col) {
+            (7..=8, _) => false,
+            _ => match turn {
+                Turn::Black => {
+                    if board.board[row + 1][col] == Cell::White {
+                        let mut count = 2;
+                        while row + count != 9 {
+                            if board.board[row + count][col] == Cell::White {
+                                count += 1;
+                                continue;
+                            } else if board.board[row + count][col] == Cell::Black {
+                                (1..count).for_each(|r| {
+                                    board.board[row + r][col] = Cell::Black;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                Turn::White => {
+                    if board.board[row + 1][col] == Cell::Black {
+                        println!("I'm here :)");
+                        let mut count = 2;
+                        while row + count != 9 {
+                            if board.board[row + count][col] == Cell::Black {
+                                count += 1;
+                                continue;
+                            } else if board.board[row + count][col] == Cell::White {
+                                (1..count).for_each(|r| {
+                                    board.board[row + r][col] = Cell::White;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                _ => true,
+            },
+        }
+    }
+
+    fn try_flipping_left(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool {
+        println!("row = {}, col = {}", row, col);
+        match (row, col) {
+            (_, 1..=2) => false,
+            _ => match turn {
+                Turn::Black => {
+                    if board.board[row][col - 1] == Cell::White {
+                        println!("I'm here :)");
+                        let mut count = 2;
+                        while col - count != 0 {
+                            if board.board[row][col - count] == Cell::White {
+                                count += 1;
+                                continue;
+                            } else if board.board[row][col - count] == Cell::Black {
+                                (1..count).for_each(|c| {
+                                    board.board[row][col - c] = Cell::Black;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                Turn::White => {
+                    if board.board[row][col - 1] == Cell::Black {
+                        println!("I'm here :)");
+                        let mut count = 2;
+                        while col - count != 0 {
+                            if board.board[row][col - count] == Cell::Black {
+                                count += 1;
+                                continue;
+                            } else if board.board[row][col - count] == Cell::White {
+                                (1..count).for_each(|c| {
+                                    board.board[row][col - c] = Cell::White;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                _ => true,
+            },
+        }
+    }
+
+    fn try_flipping_right(board: &mut Board, turn: Turn, row: usize, col: usize) -> bool {
+        println!("row = {}, col = {}", row, col);
+        match (row, col) {
+            (_, 7..=8) => false,
+            _ => match turn {
+                Turn::Black => {
+                    if board.board[row][col + 1] == Cell::White {
+                        let mut count = 2;
+                        while col + count != 9 {
+                            if board.board[row][col + count] == Cell::White {
+                                count += 1;
+                                continue;
+                            } else if board.board[row][col + count] == Cell::Black {
+                                (1..count).for_each(|c| {
+                                    board.board[row][col + c] = Cell::Black;
+                                });
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                        false
+                    } else {
+                        false
+                    }
+                }
+                Turn::White => {
+                    if board.board[row][col + 1] == Cell::Black {
+                        println!("I'm here :)");
+                        let mut count = 2;
+                        while col + count != 9 {
+                            if board.board[row][col + count] == Cell::Black {
+                                count += 1;
+                                continue;
+                            } else if board.board[row][col + count] == Cell::White {
+                                (1..count).for_each(|c| {
+                                    board.board[row][col + c] = Cell::White;
                                 });
                                 return true;
                             } else {
